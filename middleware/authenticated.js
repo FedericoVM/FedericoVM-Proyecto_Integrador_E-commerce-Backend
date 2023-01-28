4c72cbfa20ef482c76a8a753b96a33cb5a554ece
Federico Valdiglesias <federicomrtl@gmail.com>
2023-01-28 15:15:58 -0300

    Se creo los middleware para la utorizacion y verifiacion del administrador

---
+++ middleware/authenticated.js
@@ -0,0 +1,37 @@
+const jwt_util = require("../utils/jwt");
+
+const autorizado = (req,res,next) => {
+  
+    if (!req.headers.authorization) {
+        return res.status(400).send({mensaje:"Error! Falta el headers de autorizacion"})
+    } else {
+        const token = req.headers.authorization.replace("Bearer ","")
+        try {
+            const payload = jwt_util.decode(token);
+            req.user = payload;
+          
+            next()
+        } catch (error) {
+            return res.status(500).send(error)
+        }
+    }
+}
+
+
+const esAdmin = (req,res,next) => {
+   
+    if (req.user.role && req.user.role == "admin") {
+        next();
+    } else {
+        return res.status(500).send({mensaje:"Esta accion solo puede realizar el administrador"})
+    }
+}
+
+
+
+module.exports = {
+    autorizado,
+    esAdmin
+} 
+
+
